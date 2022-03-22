b257dabf31e7a598f28a750a54d2b61d63331383
Nikolai Fomin <kk309@mail.ru>
2022-03-22 12:15:08 +0300

    replace 'button' with 'link'

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -10,9 +10,9 @@ const Footer: FC = (): JSX.Element => {
         <div className="Footer__content">
           <p className="Footer__info">© 2016-2019 «Need for drive»</p>
 
-          <button type="submit" className="Footer__phone">
+          <a href="#" className="Footer__phone">
             8 (495) 234-22-44
-          </button>
+          </a>
         </div>
       </Container>
     </footer>
