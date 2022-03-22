a5518aaaa8d21d1f7a1531d4ebee984b744e0d31
Nikolai Fomin <kk309@mail.ru>
2022-03-22 12:05:49 +0300

    replace 'react-helmet' with 'react-helmet-async'
    preview virsion calls error

--- src/index.tsx
+++ src/index.tsx
@@ -2,17 +2,20 @@ import React from "react"
 import { Provider } from "react-redux"
 import { BrowserRouter } from "react-router-dom"
 import ReactDOM from "react-dom"
+import { HelmetProvider } from "react-helmet-async"
 import App from "./components/App/App"
 
 import { store } from "./store"
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Provider store={store}>
-        <App />
-      </Provider>
-    </BrowserRouter>
+    <HelmetProvider>
+      <BrowserRouter basename={window.location.pathname || ""}>
+        <Provider store={store}>
+          <App />
+        </Provider>
+      </BrowserRouter>
+    </HelmetProvider>
   </React.StrictMode>,
   document.getElementById("root")
 )
