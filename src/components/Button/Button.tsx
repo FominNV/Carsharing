396a4756fe0d17b58b796fb5199c3b233c36b5c2
Nikolai Fomin <kk309@mail.ru>
2022-03-22 12:35:37 +0300

    maлe the 'div' block self-closing

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -6,7 +6,7 @@ import "./Button.scss"
 const Button: FC<IButtonProps> = ({ name, color, borderRadius, bgColor }) => {
   return (
     <button className={`Button ${color} ${borderRadius} ${bgColor}`}>
-      <div className="Button__filter"></div>
+      <div className="Button__filter" />
       <div className="Button__name">{name}</div>
     </button>
   )
