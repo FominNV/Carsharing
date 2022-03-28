1775aae9ef628098c83367230a46928e0751e30d
Nikolai Fomin <kk309@mail.ru>
2022-03-28 16:08:00 +0300

    add path to order page

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,13 +1,15 @@
 import { FC } from "react"
 import { Route, Routes } from "react-router-dom"
 import Main from "../../pages/Main/Main"
+import Order from "../../pages/Order/Order"
 
 import "./App.scss"
 
 const App: FC = (): JSX.Element => {
   return (
     <Routes>
-      <Route path="/" element={<Main />} />
+      <Route path="/carsharing" element={<Main />} />
+      <Route path="/carsharing/order/:id" element={<Order />} />
     </Routes>
   )
 }
