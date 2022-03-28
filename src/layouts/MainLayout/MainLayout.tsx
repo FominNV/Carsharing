652cfe45c5311c91926c8ed5d78d8b53d44cd1ed
Nikolai Fomin <kk309@mail.ru>
2022-03-28 16:22:23 +0300

    add tiitle property

--- src/layouts/MainLayout/MainLayout.tsx
+++ src/layouts/MainLayout/MainLayout.tsx
@@ -1,20 +1,26 @@
 import { FC } from "react"
+import { Helmet } from "react-helmet-async"
 import Header from "../../components/Header/Header"
 import NavBar from "../../components/NavBar/NavBar"
 import { IMainLayoutProps } from "./MainLayoutTypes"
 
 import "./MainLayout.scss"
 
-const MainLayout: FC<IMainLayoutProps> = ({ children }): JSX.Element => {
+const MainLayout: FC<IMainLayoutProps> = ({ children, title }): JSX.Element => {
   return (
-    <div className="MainLayout">
-      <NavBar />
+    <>
+      <Helmet>
+        <title>{title}</title>
+      </Helmet>
+      <div className="MainLayout">
+        <NavBar />
 
-      <div className="MainLayout__content">
-        <Header />
-        {children}
+        <div className="MainLayout__content">
+          <Header />
+          {children}
+        </div>
       </div>
-    </div>
+    </>
   )
 }
 
--- src/layouts/MainLayout/MainLayoutTypes.ts
+++ src/layouts/MainLayout/MainLayoutTypes.ts
@@ -2,4 +2,5 @@ import { ReactNode } from "react"
 
 export interface IMainLayoutProps {
   children: ReactNode
+  title: string
 }
