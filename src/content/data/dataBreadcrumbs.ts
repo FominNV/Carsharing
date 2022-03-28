022dc9c43c41b3869a731b30ca361fec83ef6954
Nikolai Fomin <kk309@mail.ru>
2022-03-28 16:17:56 +0300

    create dataBreadcrumbs

---
+++ src/content/data/dataBreadcrumbs.ts
@@ -0,0 +1,22 @@
+import { IBreadcrumb } from "./dataTypes"
+
+const dataBreadcrumbs: IBreadcrumb[] = [
+  {
+    title: "Местоположение",
+    path: "place"
+  },
+  {
+    title: "Модель",
+    path: "model"
+  },
+  {
+    title: "Дополнительно",
+    path: "extra"
+  },
+  {
+    title: "Итого",
+    path: "total"
+  }
+]
+
+export default dataBreadcrumbs
