bef9dbfe47eaa1fa9f5a202a3ad0601983317191
Nikolai Fomin <kk309@mail.ru>
2022-03-29 18:50:21 +0300

    add propery setState and a function to search for a transferred вфеф

--- src/components/OrderBlock/OrderInput/OrderInput.Types.ts
+++ src/components/OrderBlock/OrderInput/OrderInput.Types.ts
@@ -1,6 +1,9 @@
+import { Dispatch, SetStateAction } from "react"
+
 export interface IOrderInputProps {
   label: string
-  defaultValue?: string
+  defaultValue: string | null
   placeholder?: string
   searchData?: string[]
+  setState: Dispatch<SetStateAction<string | null>>
 }
--- src/components/OrderBlock/OrderInput/OrderInput.tsx
+++ src/components/OrderBlock/OrderInput/OrderInput.tsx
@@ -1,4 +1,4 @@
-import React, { FC, ReactNode, useEffect, useState } from "react"
+import React, { FC, ReactNode, useEffect, useRef, useState } from "react"
 import { IOrderInputProps } from "./OrderInput.Types"
 
 import { ReactComponent as Clear } from "../../../content/icons/OrderInput/clear.svg"
@@ -9,9 +9,13 @@ const OrderInput: FC<IOrderInputProps> = ({
   label,
   placeholder,
   defaultValue,
-  searchData
+  searchData,
+  setState
 }): JSX.Element => {
   const [value, setValue] = useState<string>("")
+  const [filteredData, setFilteredData] = useState<string[] | null>(null)
+  const [showSearchBlock, setShowSearchBlock] = useState<boolean>(false)
+  const input = useRef<HTMLInputElement | null>(null)
 
   const onChangeHandler = (e: React.ChangeEvent<HTMLInputElement>): void => {
     setValue(e.currentTarget.value)
@@ -19,21 +23,74 @@ const OrderInput: FC<IOrderInputProps> = ({
 
   const clearInputValue = (e: React.MouseEvent<HTMLButtonElement>): void => {
     setValue("")
+    setState(null)
+    input.current?.focus()
   }
 
-  const createSearchBlock = (data: string[]): ReactNode => {
-    return data.map((elem: string, i: number) => (
-      <div className="OrderInput__search-block_item" key={i}>
-        {elem}
-      </div>
-    ))
+  const onMouseDownHandler = (e: React.MouseEvent<HTMLButtonElement>): void => {
+    if (e.currentTarget.name === "нет совпадений") {
+      setShowSearchBlock(false)
+      return
+    }
+
+    setValue(e.currentTarget.name)
+    setShowSearchBlock(false)
+  }
+
+  const onFocusHandler = (e: React.FocusEvent<HTMLInputElement>): void => {
+    setShowSearchBlock(true)
+  }
+
+  const onBlurHandler = (e: React.FocusEvent<HTMLInputElement>): void => {
+    setState(value)
+    setShowSearchBlock(false)
+  }
+
+  const onKeypressHandler = (e: React.KeyboardEvent<HTMLInputElement>): void => {
+    if (e.key === "Enter") {
+      setState(value)
+      setShowSearchBlock(false)
+    }
+  }
+
+  const createDataBlock = (data: string[]): ReactNode => {
+    return data.map((elem: string, i: number) => {
+      return (
+        <button
+          className="OrderInput__data-block__btn"
+          key={i}
+          name={elem}
+          onMouseDown={onMouseDownHandler}
+        >
+          {elem}
+        </button>
+      )
+    })
+  }
+
+  const filterData = (data: string[]): string[] => {
+    const filtered = data.filter((elem: string) => {
+      return elem.toLowerCase().includes(value.toLowerCase())
+    })
+
+    return filtered.length > 0 ? filtered : ["нет совпадений"]
   }
 
   useEffect(() => {
     if (defaultValue) {
       setValue(defaultValue)
+    } else if (defaultValue === null) {
+      setValue("")
     }
-  }, [])
+  }, [defaultValue])
+
+  useEffect(() => {
+    if (value && searchData) {
+      setFilteredData(filterData(searchData))
+    } else if (!value && searchData) {
+      setFilteredData(null)
+    }
+  }, [value])
 
   return (
     <div className="OrderInput">
@@ -47,8 +104,12 @@ const OrderInput: FC<IOrderInputProps> = ({
           className="OrderInput__inp"
           value={value}
           name={label}
+          ref={input}
           placeholder={placeholder}
           onChange={onChangeHandler}
+          onFocus={onFocusHandler}
+          onBlur={onBlurHandler}
+          onKeyDown={onKeypressHandler}
         />
 
         <button
@@ -58,8 +119,12 @@ const OrderInput: FC<IOrderInputProps> = ({
           <Clear />
         </button>
 
-        <div className="OrderInput__search-block">
-          {searchData && createSearchBlock(searchData)}
+        <div
+          className={`OrderInput__data-block ${
+            showSearchBlock ? "OrderInput__data-block_active" : ""
+          }`}
+        >
+          {filteredData ? createDataBlock(filteredData) : searchData && createDataBlock(searchData)}
         </div>
       </div>
     </div>
