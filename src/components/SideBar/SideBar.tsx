24a130434e92919f058a166acd7c39b9823b6afb
Nikolai Fomin <kk309@mail.ru>
2022-03-29 18:54:56 +0300

    add displaing order place

--- src/components/SideBar/SideBar.tsx
+++ src/components/SideBar/SideBar.tsx
@@ -1,23 +1,32 @@
 import { FC } from "react"
+import { useTypedSelector } from "../../store/selectors"
+import OrderPoint from "../OrderBlock/OrderPoint/OrderPoint"
 import Button from "../Button/Button"
 import { ButtonBgColor } from "../Button/ButtonTypes"
-import OrderPoint from "../OrderBlock/OrderPoint/OrderPoint"
 
 import "./SideBar.scss"
 
 const SideBar: FC = (): JSX.Element => {
+  const { place } = useTypedSelector((state) => state.order)
+
   return (
     <div className="SideBar">
       <h3 className="SideBar__title">Ваш заказ:</h3>
-      <div className="SideBar__order-points"></div>
-      <OrderPoint
-        title="Пункт выдачи"
-        value="Ульяновск,
-          Нариманова&nbsp;42"
-      />
+
+      <div className="SideBar__order-points">
+        {place.city && place.street && (
+          <OrderPoint title="Пункт выдачи" firstValue={place.city} secondValue={place.street} />
+        )}
+      </div>
+
       <p className="SideBar__total-price">Цена: от 8 000 до 12 000 ₽</p>
+
       <div className="SideBar__btn">
-        <Button name="Выбрать модель" bgColor={ButtonBgColor.GREEN} disabled />
+        <Button
+          name="Выбрать модель"
+          bgColor={ButtonBgColor.GREEN}
+          disabled={place.street === null}
+        />
       </div>
     </div>
   )
