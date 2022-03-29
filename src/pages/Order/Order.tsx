5d6a0742257d6a89285de1bf8ff479f2f2bc6d82
Nikolai Fomin <kk309@mail.ru>
2022-03-29 18:58:39 +0300

    refactor code

--- src/pages/Order/Order.tsx
+++ src/pages/Order/Order.tsx
@@ -1,8 +1,8 @@
 import { FC } from "react"
-import Container from "../../components/Container/Container"
-import Place from "../../components/OrderBlock/Place/Place"
-import SideBar from "../../components/SideBar/SideBar"
 import OrderLayout from "../../layouts/OrderLayout/OrderLayout"
+import Container from "../../components/Container/Container"
+import SideBar from "../../components/SideBar/SideBar"
+import Place from "../../components/OrderBlock/Place/Place"
 
 import "./Order.scss"
 
