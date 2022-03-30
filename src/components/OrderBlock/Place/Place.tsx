f004a2d6ab509833f0709f696ccbaf2d0f11dad9
Nikolai Fomin <kk309@mail.ru>
2022-03-30 17:17:33 +0300

    add yandex map and markers

--- src/components/OrderBlock/Place/Place.tsx
+++ src/components/OrderBlock/Place/Place.tsx
@@ -2,53 +2,88 @@ import { FC, useEffect, useState } from "react"
 import { useDispatch } from "react-redux"
 import { useTypedSelector } from "../../../store/selectors"
 import { setPlaceCity, setPlaceStreet } from "../../../store/order/actions"
-import { IDataSearchPlace } from "../../../content/data/dataTypes"
+import { IDataOrderPlace, IPlace } from "../../../content/data/dataTypes"
+import { IMapState, IMarker } from "../OrderMap/OrderMapTypes"
 import OrderInput from "../OrderInput/OrderInput"
+import OrderMap from "../OrderMap/OrderMap"
 
-import map from "../../../content/images/Order/map.jpg"
-import dataSearchPlace from "../../../content/data/dataSearchPlace"
+import dataOrderPlace from "../../../content/data/dataOrderPlace"
 
 import "./Place.scss"
 
 const Place: FC = (): JSX.Element => {
-  const { place } = useTypedSelector((state) => state.order)
-  const [city, setCity] = useState<string | null>(null)
+  const { order, common } = useTypedSelector((state) => state)
+  const [city, setCity] = useState<string | null>(common.city ? common.city : null)
   const [street, setStreet] = useState<string | null>(null)
+  const [mapState, setMapState] = useState<IMapState>({ center: [55.355198, 86.086847], zoom: 10 })
 
   const dispatch = useDispatch()
 
-  const getCitiesFromData = (data: IDataSearchPlace[]): string[] => {
-    return data.map((elem: IDataSearchPlace) => elem.city)
+  const getCitiesFromData = (data: IDataOrderPlace[]): string[] => {
+    return data.map((elem: IDataOrderPlace) => elem.city.name)
   }
 
   const getStreetsFromData = (
-    data: IDataSearchPlace[],
+    data: IDataOrderPlace[],
     currentCity: string | null = null
   ): string[] => {
-    const result: string[] = []
+    const array: string[] = []
 
     if (currentCity) {
       return data
-        .filter((elem: IDataSearchPlace) => elem.city === currentCity)
-        .reduce((prev, current: IDataSearchPlace) => prev.concat(current.streets), result)
+        .filter((elem: IDataOrderPlace) => elem.city.name === currentCity)
+        .reduce((prev, current: IDataOrderPlace) => {
+          return prev.concat(current.streets.map((elem) => elem.name))
+        }, array)
     }
 
-    return data.reduce((prev, current: IDataSearchPlace) => prev.concat(current.streets), result)
+    return data.reduce(
+      (prev, current: IDataOrderPlace) => prev.concat(current.streets.map((elem) => elem.name)),
+      array
+    )
   }
 
-  const setPlaceByStreet = (value: string, data: IDataSearchPlace[]): void => {
-    data.map((elem: IDataSearchPlace) => {
-      if (elem.streets.includes(value)) {
-        dispatch(setPlaceCity(elem.city))
-        dispatch(setPlaceStreet(value))
+  const createMarkersData = (data: IDataOrderPlace[]): IMarker[] => {
+    const array: IMarker[] = []
+    return data.reduce(
+      (prev, current: IDataOrderPlace) => prev.concat(current.streets.map((elem) => elem)),
+      array
+    )
+  }
+
+  const setPlaceByStreet = (currentStreet: string, data: IDataOrderPlace[]): void => {
+    data.map((elem: IDataOrderPlace) => {
+      elem.streets.map((item) => {
+        if (item.name === currentStreet) {
+          dispatch(setPlaceCity(elem.city.name))
+          dispatch(setPlaceStreet(currentStreet))
+        }
+      })
+    })
+  }
+
+  const showCityOnMap = (town: string, data: IDataOrderPlace[]): void => {
+    data.map((elem) => {
+      if (elem.city.name === town) {
+        setMapState({ center: elem.city.coord, zoom: 10 })
       }
-      return false
+    })
+  }
+
+  const showOrderPlaceOnMap = (address: string, data: IDataOrderPlace[]): void => {
+    data.map((elem: IDataOrderPlace) => {
+      elem.streets.map((item: IPlace) => {
+        if (item.name === address) {
+          setMapState({ center: item.coord, zoom: 17 })
+        }
+      })
     })
   }
 
   useEffect(() => {
     if (street) {
-      setPlaceByStreet(street, dataSearchPlace)
+      setPlaceByStreet(street, dataOrderPlace)
+      showOrderPlaceOnMap(street, dataOrderPlace)
     } else {
       dispatch(setPlaceStreet(null))
     }
@@ -62,8 +97,14 @@ const Place: FC = (): JSX.Element => {
   }, [city])
 
   useEffect(() => {
-    if (place.city && place.city !== city) {
-      setCity(place.city)
+    if (city) {
+      showCityOnMap(city, dataOrderPlace)
+    }
+  }, [city])
+
+  useEffect(() => {
+    if (order.place.city && order.place.city !== city) {
+      setCity(order.place.city)
     }
   }, [city])
 
@@ -73,24 +114,30 @@ const Place: FC = (): JSX.Element => {
         <OrderInput
           label="Город"
           placeholder="Введите город"
-          defaultValue={place.city}
-          searchData={getCitiesFromData(dataSearchPlace)}
+          value={order.place.city}
+          defaultValue={common.city}
+          searchData={getCitiesFromData(dataOrderPlace)}
           setState={setCity}
         />
         <OrderInput
           label="Пункт выдачи"
           placeholder="Начните вводить пункт ..."
-          defaultValue={place.street}
-          searchData={
-            city ? getStreetsFromData(dataSearchPlace, city) : getStreetsFromData(dataSearchPlace)
-          }
+          value={order.place.street}
+          searchData={getStreetsFromData(dataOrderPlace, city)}
           setState={setStreet}
         />
       </div>
 
       <div className="Place__map-wrap">
         <p className="Place__map-text">Выбрать на карте:</p>
-        <img className="Place__map" src={map} alt="map" />
+
+        <div className="Place__map">
+          <OrderMap
+            mapState={mapState}
+            markerData={createMarkersData(dataOrderPlace)}
+            setState={setStreet}
+          />
+        </div>
       </div>
     </div>
   )
