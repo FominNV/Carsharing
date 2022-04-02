9d5911cf0da642c1f100ce450ec2e63f7cdb0cff
Nikolai Fomin <kk309@mail.ru>
2022-04-03 00:16:21 +0300

    V-02 correct className with classNames library

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,13 +1,18 @@
 import React, { FC } from "react"
+import classNames from "classnames"
 import { IButtonProps } from "./types"
 
 import "./styles.scss"
 
-const Button: FC<IButtonProps> = ({ name, color, borderRadius, bgColor, disabled }) => (
-  <button className={`Button ${color} ${borderRadius} ${bgColor}`} disabled={disabled}>
-    <div className="Button__filter" />
-    <div className="Button__name">{name}</div>
-  </button>
-)
+const Button: FC<IButtonProps> = ({ name, color, borderRadius, bgColor, disabled }) => {
+  const btnClassName = classNames("Button", color, borderRadius, bgColor)
+
+  return (
+    <button className={btnClassName} disabled={disabled}>
+      <div className="Button__filter" />
+      <div className="Button__name">{name}</div>
+    </button>
+  )
+}
 
 export default React.memo(Button)
