440c35545ed9d2eefbfb7d25830ff2997b8e3530
Nikolai Fomin <kk309@mail.ru>
2022-04-02 23:43:48 +0300

    V-02 reorganize project folders

---
+++ src/components/Breadcrumbs/data.ts
@@ -0,0 +1,26 @@
+import { IBreadcrumb } from "./types"
+
+const dataBreadcrumbs: IBreadcrumb[] = [
+  {
+    id: "Breadcrumb_1",
+    title: "Местоположение",
+    path: "place"
+  },
+  {
+    id: "Breadcrumb_2",
+    title: "Модель",
+    path: "model"
+  },
+  {
+    id: "Breadcrumb_3",
+    title: "Дополнительно",
+    path: "extra"
+  },
+  {
+    id: "Breadcrumb_4",
+    title: "Итого",
+    path: "total"
+  }
+]
+
+export default dataBreadcrumbs
