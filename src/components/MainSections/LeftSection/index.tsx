78e7b72bc9ea4a2bbdfd96bd3360a9e917e52b24
Nikolai Fomin <kk309@mail.ru>
2022-04-05 13:49:50 +0300

    V-02 remove useless folders and files

--- src/components/MainSections/LeftSection/index.tsx
+++
@@ -1,7 +0,0 @@
-import { FC } from "react"
-
-import "./styles.scss"
-
-const LeftSection: FC = ({ children }) => <section className="LeftSection">{children}</section>
-
-export default LeftSection
--- src/components/MainSections/RightSection/index.tsx
+++
@@ -1,7 +0,0 @@
-import { FC } from "react"
-
-import "./styles.scss"
-
-const RightSection: FC = ({ children }) => <section className="RightSection">{children}</section>
-
-export default RightSection
