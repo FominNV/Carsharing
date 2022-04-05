846dca81fa7ecf3894db5b03d74d40979477a7f7
Nikolai Fomin <kk309@mail.ru>
2022-04-05 12:39:04 +0300

    Update index.tsx

--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -1,4 +1,4 @@
-import { FC } from "react"
+import { FC, useMemo } from "react"
 import { useDispatch } from "react-redux"
 import { useTypedSelector } from "store/selectors"
 import { setLanguage } from "store/common/actions/setLanguage"
@@ -18,37 +18,43 @@ const NavBar: FC = () => {
   const { showMenu, rusLang } = useTypedSelector((state) => state.common)
   const dispatch = useDispatch()
 
-  const toggleLanguage: MouseEventFunc<HTMLButtonElement> = () => {
+  const toggleLanguage: EventFunc<MouseEvent> = () => {
     dispatch(setLanguage(!rusLang))
   }
 
-  const toggleMenu: MouseEventFunc<HTMLButtonElement> = () => {
+  const toggleMenu: EventFunc<MouseEvent> = () => {
     dispatch(setDisplayMenu(!showMenu))
   }
 
-  const navbarClassName = classNames("NavBar__menu", { NavBar__menu_active: showMenu })
-  const langClassName = classNames("NavBar__lang", { NavBar__lang_active: showMenu })
-
-  const menuIcon = showMenu ? (
+  const menuIcon = useMemo<JSX.Element>(() => (showMenu ? (
     <Close className="NavBar__menu_icon" />
   ) : (
     <Burger className="NavBar__menu_icon" />
-  )
-  const langIcon = rusLang ? <Rus /> : <Eng />
-  const popuMenu = showMenu && <PopupMenu />
+  )), [showMenu])
+
+  const langIcon = useMemo<JSX.Element>(() => (rusLang ? <Rus /> : <Eng />), [rusLang])
+  const popuMenu = useMemo<JSX.Element | false>(() => (showMenu && <PopupMenu />), [showMenu])
+
+  const navbarClassName = classNames("NavBar__menu", { NavBar__menu_active: showMenu })
+  const langClassName = classNames("NavBar__lang", { NavBar__lang_active: showMenu })
 
   return (
     <>
       {popuMenu}
       <nav className="NavBar">
-        <button className={navbarClassName} onClick={toggleMenu}>
+        <button
+          className={navbarClassName}
+          onClick={toggleMenu}
+        >
           {menuIcon}
         </button>
 
         <div className={langClassName}>
-          <button className="NavBar__lang_btn" onClick={toggleLanguage}>
+          <button
+            className="NavBar__lang_btn"
+            onClick={toggleLanguage}
+          >
             <div className="NavBar__lang_name">{langIcon}</div>
-
             <div className="NavBar__lang_circle">
               <Circle />
             </div>
