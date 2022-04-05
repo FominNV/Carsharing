ce9c621bc3530fbf014305f649ca925c3691c296
Nikolai Fomin <kk309@mail.ru>
2022-04-05 12:55:38 +0300

    V-02 add const for paths and rename sections

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -5,16 +5,18 @@ import Button from "components/Button"
 import Footer from "components/Footer"
 import MainLayout from "layouts/MainLayout"
 import Container from "components/Container"
-import LeftSection from "components/MainSections/LeftSection"
-import RightSection from "components/MainSections/RightSection"
+import LandSection from "components/MainSections/LandSection"
+import SliderSection from "components/MainSections/SliderSection"
 import Slider from "components/SliderBlock/Slider"
+
 import { ButtonBgColor } from "components/Button/types"
+import { PATHS } from "routes/consts"
 
 import "./styles.scss"
 
 const Main: FC = () => (
   <div className="Main">
-    <LeftSection>
+    <LandSection>
       <MainLayout title="NFD">
         <main className="Main__main">
           <Container>
@@ -24,8 +26,11 @@ const Main: FC = () => (
               <p className="Main__tagline3">Поминутная аренда авто твоего города</p>
 
               <div className="Main__btn">
-                <Link to="order/place">
-                  <Button name="Забронировать" bgColor={ButtonBgColor.GREEN} />
+                <Link to={PATHS.ORDER_PLACE}>
+                  <Button
+                    name="Забронировать"
+                    bgColor={ButtonBgColor.GREEN}
+                  />
                 </Link>
               </div>
             </div>
@@ -33,11 +38,11 @@ const Main: FC = () => (
         </main>
         <Footer />
       </MainLayout>
-    </LeftSection>
+    </LandSection>
 
-    <RightSection>
+    <SliderSection>
       <Slider />
-    </RightSection>
+    </SliderSection>
   </div>
 )
 
