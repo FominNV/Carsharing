0092238204b0c61840f6d0f0f3af5bf98f4bb003
Nikolai Fomin <kk309@mail.ru>
2022-04-05 12:53:51 +0300

    F-02 add const PATHS

---
+++ src/routes/consts.ts
@@ -0,0 +1,7 @@
+export const PATHS = {
+  MAIN: '/carsharing',
+  ORDER_PLACE: '/carsharing/order/place',
+  ORDER_CAR: '/carsharing/order/car',
+  ORDER_EXTRA: '/carsharing/order/extra',
+  ORDER_TOTAL: '/carsharing/order/total'
+}
