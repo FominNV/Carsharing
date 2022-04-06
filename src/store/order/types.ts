37a6b27d06791c278d829fac8099228cda0f2574
Nikolai Fomin <kk309@mail.ru>
2022-04-06 19:23:39 +0300

    F-02 add switching between order steps

--- src/store/order/types.ts
+++ src/store/order/types.ts
@@ -1,15 +1,53 @@
 export interface IOrderState {
-  place: {
-    city: Nullable<string>
-    street: Nullable<string>
-  }
+  [index: string]:
+    | IOrderPlace
+    | Nullable<IOrderCar>
+    | Nullable<IOrderExtra>
+    | Nullable<IOrderTotal>
+    | IUnlockeOrderStep
+  place: IOrderPlace
+  car: Nullable<IOrderCar>
+  extra: Nullable<IOrderExtra>
+  total: Nullable<IOrderTotal>
+  unlockedStep: IUnlockeOrderStep
+}
+
+export interface IOrderPlace {
+  city: Nullable<string>
+  street: Nullable<string>
+}
+
+export interface IOrderCar {
+  id: string
+  name: string
+  priceMin: number
+  priceMax: number
+}
+
+export interface IOrderExtra {
+  color: Nullable<string>
+}
+
+export interface IOrderTotal {
+  order: Nullable<string>
+}
+
+export interface IUnlockeOrderStep {
+  [index: string]: boolean
+  place:boolean
+  car: boolean
+  extra: boolean
+  total: boolean
 }
 
 export type OrderDispatch<T> = (value: T) => OrderAction
+export type SetLockStepType = (step: keyof IUnlockeOrderStep, lock: boolean) => OrderAction
 
 export enum OrderActionTypes {
   SET_CITY = "SET_CITY",
-  SET_STREET = "SET_STREET"
+  SET_STREET = "SET_STREET",
+  SET_ORDER_CAR = "SET_ORDER_CAR",
+  SET_LOCK_STEP = "SET_LOCK_STEP"
 }
 
 type SetCityAction = {
@@ -22,4 +60,14 @@ type SetStreetAction = {
   payload: { street: Nullable<string> }
 }
 
-export type OrderAction = SetCityAction | SetStreetAction
+type SetCarAction = {
+  type: OrderActionTypes.SET_ORDER_CAR
+  payload: { car: Nullable<IOrderCar> }
+}
+
+type SetLockStepAction = {
+  type: OrderActionTypes.SET_LOCK_STEP
+  payload: { step: keyof IUnlockeOrderStep; lock: boolean }
+}
+
+export type OrderAction = SetCityAction | SetStreetAction | SetCarAction | SetLockStepAction
