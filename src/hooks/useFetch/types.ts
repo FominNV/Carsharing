d4a7fdc819bc89eaa4900ab4be730b96c5c98ee8
Nikolai Fomin <kk309@mail.ru>
2022-04-23 14:37:02 +0300

    F-03, F-04 create hook useFetch

---
+++ src/hooks/useFetch/types.ts
@@ -0,0 +1,35 @@
+import { ICar, ICategory } from "store/car/types"
+import { IPoint } from "store/location/types"
+
+export interface IFetchOptions {
+  method: FetchMethod
+  headers: {
+    "X-Api-Factory-Application-Id": string
+    "Content-Type"?: string
+  }
+}
+
+export interface IFetchData {
+  data: ICar[] | ICategory[] | IPoint[]
+}
+
+export interface IFetchState {
+  data: Nullable<IFetchData>
+  error: Nullable<Error>
+}
+
+export type UseFetchType = (url: string, option: IFetchOptions) => Promise<IFetchState>
+
+export enum URLS {
+  POINT_URL = "https:///api-factory.simbirsoft1.com/api/db/point/",
+  CAR_URL = "https:///api-factory.simbirsoft1.com/api/db/car/",
+  CATEGORY_URL = "https:///api-factory.simbirsoft1.com/api/db/category/",
+  RATE_URL = "https:///api-factory.simbirsoft1.com/api/db/rate/",
+  ORDER_URL = "https:///api-factory.simbirsoft1.com/api/db/order/",
+  ORDER_STATUS_URL = "https:///api-factory.simbirsoft1.com/api/db/orderStatus/"
+}
+
+export enum FetchMethod {
+  GET = "GET",
+  POST = "POST"
+}
