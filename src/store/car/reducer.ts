e8ba6e970fb38f0281a6dcb2d34670b2b04683f9
Nikolai Fomin <kk309@mail.ru>
2022-04-23 14:39:57 +0300

    F-04 add fetching cars ang categories

---
+++ src/store/car/reducer.ts
@@ -0,0 +1,36 @@
+import { CarAction, CarActionTypes, ICarState } from "./types"
+
+const initialState: ICarState = {
+  cars: {
+    all: null
+  },
+  category: {
+    all: null,
+    current: null
+  }
+}
+
+export function carReducer(state: ICarState = initialState, action: CarAction): ICarState {
+  switch (action.type) {
+    case CarActionTypes.GET_CARS:
+      return {
+        ...state,
+        cars: { ...state.cars, all: action.payload.cars }
+      }
+
+    case CarActionTypes.GET_CATEGORIES:
+      return {
+        ...state,
+        category: { ...state.category, all: action.payload.categories }
+      }
+
+    case CarActionTypes.SET_CURRENT_CATEGORY:
+      return {
+        ...state,
+        category: { ...state.category, current: action.payload.current }
+      }
+
+    default:
+      return state
+  }
+}
