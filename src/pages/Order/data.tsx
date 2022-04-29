b841eeb8ea292993ed533f6c039135fd14834cea
Nikolai Fomin <kk309@mail.ru>
2022-04-29 17:01:54 +0300

    V-05-06, F-06-07 add functionality

--- src/pages/Order/data.tsx
+++ src/pages/Order/data.tsx
@@ -1,7 +1,10 @@
 import Place from "components/OrderBlock/Steps/Place"
 import Car from "components/OrderBlock/Steps/Car"
 import Extra from "components/OrderBlock/Steps/Extra"
-import { IOrderStep } from "./types"
+import Total from "components/OrderBlock/Steps/Total"
+import Ordered from "components/OrderBlock/Steps/Ordered"
+import Canceled from "components/OrderBlock/Steps/Canceled"
+import { IDataPageTitleItem, IDataStatusItem, IOrderStep } from "./types"
 
 export const dataOrderSteps: IOrderStep[] = [
   {
@@ -15,5 +18,59 @@ export const dataOrderSteps: IOrderStep[] = [
   {
     id: "extra",
     component: <Extra />
+  },
+  {
+    id: "total",
+    component: <Total />
+  },
+  {
+    id: "ordered",
+    component: <Ordered />
+  },
+  {
+    id: "canceled",
+    component: <Canceled />
+  }
+]
+
+export const dataOrderStatuses: IDataStatusItem[] = [
+  {
+    key: "new",
+    status: "Новые"
+  },
+  {
+    key: "confirm",
+    status: "Подтвержденные"
+  },
+  {
+    key: "cancel",
+    status: "Отмененые"
+  }
+]
+
+export const dataPageTitles: IDataPageTitleItem[] = [
+  {
+    id: "place",
+    title: "NFD / Выбор места"
+  },
+  {
+    id: "car",
+    title: "NFD / Выбор авто"
+  },
+  {
+    id: "extra",
+    title: "NFD / Выбор услуг"
+  },
+  {
+    id: "total",
+    title: "NFD / Итого"
+  },
+  {
+    id: "ordered",
+    title: "NFD / Ваш заказ"
+  },
+  {
+    id: "canceled",
+    title: "NFD / Отмена"
   }
 ]
